fe9c34d5e3445057108836fd4864a9f67c1a1b14
alexc83 <alex.cruce@gmail.com>
2025-02-21 23:21:38 -0600

    initial commit

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Batting practice for an hour a day";
+    }
+}
